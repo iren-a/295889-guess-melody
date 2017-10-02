3f4c9d960468976e03bf39bfc8b0c634fad3482c
Irina Arinushkina <ira.arinushkina@gmail.com>
2017-10-02 22:39:36 +0300

    #4 Тесные связи

--- js/main-result-time-over.js
+++ js/main-result-time-over.js
@@ -1,7 +1,22 @@
-import {getElementbyTemplate} from "./utils";
+import {getElementbyTemplate, renderScreen} from "./utils";
+import mainWelcomeElement from "./main-welcome";
 
-const template = document.querySelector(`#templates`).content.querySelectorAll(`.main--result`)[1].outerHTML;;
+const template = `<section class="main main--result">
+    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+
+    <h2 class="title">Увы и ах!</h2>
+    <div class="main-stat">Время вышло!<br>Вы не успели отгадать все мелодии</div>
+    <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
+  </section>`;
 
 const mainResultTimeOver = getElementbyTemplate(template);
+
+const replayButton = mainResultTimeOver.querySelector(`.main-replay`);
+
+const replayButtonClickHandler = () => {
+  renderScreen(mainWelcomeElement);
+};
+
+replayButton.addEventListener(`click`, replayButtonClickHandler);
 
 export default mainResultTimeOver;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-const leftKeyCode = 37;
-const rightKeyCode = 39;
-
 import {renderScreen} from "./utils";
 import mainWelcomeElement from "./main-welcome";
 
 renderScreen(mainWelcomeElement);
-
-export const playButton = document.querySelector(`.main-play`);
